99446fa47bda80d4ac80ea52de5a09a1692e18fd
akagamina <omerdogan0@gmail.com>
2022-08-17 00:58:18 +0300

    ee6a7a1

---
+++ index.js
@@ -0,0 +1,50 @@
+import img00 from '@/assets/img/product/00-0.webp'
+import img01 from '@/assets/img/product/00-1.webp'
+import img10 from '@/assets/img/product/01-0.webp'
+import img11 from '@/assets/img/product/01-1.webp'
+import img20 from '@/assets/img/product/02-0.webp'
+import img21 from '@/assets/img/product/02-1.webp'
+import img30 from '@/assets/img/product/03-0.webp'
+import img31 from '@/assets/img/product/03-1.webp'
+import img40 from '@/assets/img/product/04-0.webp'
+import img41 from '@/assets/img/product/04-1.webp'
+
+const products = [
+	{
+		id: 1,
+		name: 'Gaberdine',
+		category: 2,
+		price: 450,
+		img: [img00, img01]
+	},
+	{
+		id: 2,
+		name: 'Button Up Shirt -M',
+		category: 4,
+		price: 160,
+		img: [img10, img11]
+	},
+	{
+		id: 5,
+		name: 'Jeans',
+		category: 3,
+		price: 140,
+		img: [img40, img41]
+	},
+	{
+		id: 4,
+		name: 'Tote Bag',
+		category: 5,
+		price: 310,
+		img: [img30, img31]
+	},
+	{
+		id: 3,
+		name: 'Button Up Shirt Blouse',
+		category: 4,
+		price: 165,
+		img: [img20, img21]
+	}
+]
+
+export default products
